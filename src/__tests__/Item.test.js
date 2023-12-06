5c3f3891428a25fe1b73d6657d3a3ca491178234
Glorfindel-Noldor <131824775+Glorfindel-Noldor@users.noreply.github.com>
2023-12-06 17:52:42 -0600

    i like class methods over functions because it pairs well with my coffee

--- src/__tests__/Item.test.js
+++ src/__tests__/Item.test.js
@@ -20,6 +20,7 @@ test("the <li> does not have a className when initialized", () => {
 
 test("the <li> has a className of 'in-cart' when the Add to Cart button is clicked", () => {
   const { container } = render(<Item name="Milk" category="Dairy" />);
-  fireEvent.click(screen.getByText(/ Cart/));
+  fireEvent.click(screen.getByText("Add to Cart")); // Make sure the button text matches exactly
   expect(container.querySelector(".in-cart")).toBeInTheDocument();
 });
+
